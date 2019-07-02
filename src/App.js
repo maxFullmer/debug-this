24f3261de749b08f3eb964c35c3bca8ee4e7ca5a
Max-F <max.fullmer@outlook.com>
2019-07-02 11:07:43 -0700

    fixed everything

--- src/App.js
+++ src/App.js
@@ -3,38 +3,56 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  contsructor(props) {
+  constructor(props) {
+    super(props)
     this.state = {
       friendsList: ["john", "jacob", "jingle", "heimer", "schmidt"],
       friend: ""
     };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addFriend = this.addFriend.bind(this);
   }
+
   changeHandler(event) {
     this.setState({
-      friend: event
+      friend: event.target.value
     });
   }
 
   addFriend() {
-    this.state.friendsList.push(this.state.friend);
-  }
+    let myUpdatedList = this.state.friendsList.slice();
+    myUpdatedList.push(this.state.friend);
+    this.setState({
+      friendsList: myUpdatedList
+    })
+
+    }
+    // this.state.friendsList.push(this.state.friend);
+  
 
   deleteFriend(index) {
-    this.state.friendsList.splice(index, 1);
+    let myUpdatedList = this.state.friendsList.slice();
+    myUpdatedList.splice(index,1);
+    this.setState({
+      friendsList: myUpdatedList
+    });
+    // this.state.friendsList.splice(index, 1);
   }
 
   render() {
     const mappedFreinds = this.state.friendsList.map((friend, index) => {
+      return (
       <div key={index}>
         <span>{friend}</span>
-        <button onClick={this.state.deleteFriend} />
-      </div>;
+        <button onClick={() => this.deleteFriend(index)}>Delete </button>
+      </div>
+    );
     });
     return (
       <div className="App">
         <div>{mappedFreinds}</div>
         <input
-          type="number"
+          type="text"
           onChange={this.changeHandler}
           value={this.state.friend}
         />
